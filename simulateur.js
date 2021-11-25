0f877ca6c1d2140b2dbdbe249fb6b5b6025d6268
Brendan Chabannes <brendan.chabannes@hey.com>
2021-11-25 09:29:25 +0100

    calcul traitement

--- simulateur.js
+++ simulateur.js
@@ -1,10 +1,12 @@
 
 const inflationDepuis1995 = 42.5;
 const augmentationIndiceDepuis1995 = 14.3;
+const ptIndiceActuel = 4.68;
 
 // Événement 
 const menuEchelon = document.querySelector('select[id="echelon"]');
 menuEchelon.addEventListener("change", () => {
   let indice = parseInt(document.getElementById('echelon').value,10);
-  console.log(indice);
+  let traitementBrutActuel = parseFloat(indice*ptIndiceActuel).toPrecision(6);
+  console.log(traitementBrutActuel);
 });
