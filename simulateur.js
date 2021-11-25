b9150379e34836d1843ad7ef1655a04d59aced90
Brendan Chabannes <brendan.chabannes@hey.com>
2021-11-25 10:27:21 +0100

    pas de calcul si pas d’échelon sélectionné

--- simulateur.js
+++ simulateur.js
@@ -6,7 +6,28 @@ const ptIndiceActuel = 4.68;
 // Événement 
 const menuEchelon = document.querySelector('select[id="echelon"]');
 menuEchelon.addEventListener("change", () => {
+  if (document.getElementById('echelon').value == "0") {
+    return;
+  }
   let indice = parseInt(document.getElementById('echelon').value,10);
-  let traitementBrutActuel = parseFloat(indice*ptIndiceActuel).toPrecision(6);
+  let traitementBrutActuel = indice * ptIndiceActuel;
   console.log(traitementBrutActuel);
+  let traitementInflationCompensee = traitementBrutActuel + traitementBrutActuel * ((inflationDepuis1995 - augmentationIndiceDepuis1995)/100);
+  console.log("Devrait être " + traitementInflationCompensee);
+  let baissePourcentage = inflationDepuis1995 - augmentationIndiceDepuis1995;
+  let manqueMensuel = traitementInflationCompensee - traitementBrutActuel;
+  let manqueAnnuel = manqueMensuel * 12;
+
 });
+
+
+
+/* 
+L'inflation depuis entre 1995 et 2019 est de XXX % 
+Le point d'indice a augmenté durant la même période de XX %
+
+Votre traitement indiciaire brut est aujourd'hui de XXXX € mensuels (hors primes).
+
+Si le point d'indice avait augmenté en suivant l'inflation depuis 1995, votre traitement devrait être de XXXX €
+
+Votre traitement brut a baissé relativement de XX% : il manque mensuellement XXXX€ / annuellement XXXX€ */
