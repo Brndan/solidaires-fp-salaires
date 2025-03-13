fdcf067ac584107071cafd23d7a9287334ef3aa0
Brendan Chabannes <brendan.chabannes@hey.com>
2025-03-13 13:53:52 +0100

    Mise à jour 2025

--- simulateur.js
+++ simulateur.js
@@ -1,7 +1,7 @@
 // <script> // pour site de Solidaires
-const inflationDepuis1995 = 55.00; // en mai 2023
-const augmentationIndiceDepuis1995 = 20.00; // en juillet 2023
-const ptIndiceActuel = 4.92; // en juillet 2023
+const inflationDepuis2000 = 52.30; // en décembre 2024
+const augmentationIndiceDepuis2000 = 15.37; // en décembre 2024
+const ptIndiceActuel = 4.92; // en décembre 2024
 
 // Événement 
 const menuEchelon = document.querySelector('select[id="echelon"]');
@@ -13,8 +13,8 @@ menuEchelon.addEventListener("change", () => {
   document.getElementById('result').style.display = 'block';
   let indice = parseInt(document.getElementById('echelon').value,10);
   let traitementBrutActuel = indice * ptIndiceActuel;
-  let traitementInflationCompensee = traitementBrutActuel + traitementBrutActuel * ((inflationDepuis1995 - augmentationIndiceDepuis1995)/100);
-  let baissePourcentage = inflationDepuis1995 - augmentationIndiceDepuis1995;
+  let traitementInflationCompensee = traitementBrutActuel + traitementBrutActuel * ((inflationDepuis2000 - augmentationIndiceDepuis2000)/100);
+  let baissePourcentage = inflationDepuis2000 - augmentationIndiceDepuis2000;
   let manqueMensuel = traitementInflationCompensee - traitementBrutActuel;
   let manqueAnnuel = manqueMensuel * 12;
   document.getElementById("traitementIndiciaireBrut").innerHTML = (Math.round(traitementBrutActuel * 100) / 100).toLocaleString() + "&nbsp;€";
